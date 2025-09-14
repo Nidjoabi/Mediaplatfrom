32633f2824971522621670b8498f685ebf289257
NIKOLA\nikog <if24b103@technikum-wien.at>
2025-09-14 16:40:34 +0200

    Test Objects erstellt

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
-    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+    public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-        ;
+
     }
+
+    User user1 = new User("nikola", "1234", "fakemail@gmail.com");
+    Movie m1 = new Movie(
+            "Inception",
+            "A mind-bending sci-fi thriller",
+            "Movie",
+            2010,
+            "Sci-Fi",
+            true,
+            "Christopher Nolan",
+            148
+    );
+
 }
