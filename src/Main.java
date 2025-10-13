0c2e44bc6d0fdaaea6dc0fa82c8753d0777fa6c5
NIdjoabi <if24b103@technikum-wien.at>
2025-10-13 17:04:17 +0200

    tests in der main

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,31 @@
-import Modules.User;
 
-import handler.Gamehandler;
 import handler.Userhandler;
 import persistence.IUserRepository;
 import persistence.UserSqlRepository;
-import persistence.IGameRepository;
-import persistence.GameSqlRepository;
 import service.IUserService;
-import service.UserService;
+
 import service.TokenService;
-import service.IGameService;
-import service.GameService;
-
-
+import service.UserService;
 
 public class Main {
     public static void main(String[] args) {
 
-        // === User Setup ===
-        IUserRepository userRepository = UserSqlRepository.getInstance();
-        TokenService tokenService = TokenService.getInstance();
-        IUserService userService = UserService.getInstance(userRepository, tokenService);
-
-        User user = new User("admin", "password", "email");
+        IUserRepository repository = UserSqlRepository.getInstance();
+        TokenService tokenService = new TokenService();
+        IUserService userService = UserService.getInstance(repository, tokenService);
+        String username = "admin";
+        String password = "password";
+        String email = "email";
 
         Userhandler userHandler = new Userhandler(userService);
-        boolean registered = userHandler.register(user.getUsername(), user.getPassword(), user.getEmail());
-        boolean loggedIn = userHandler.login(user.getUsername(), user.getPassword());
+        boolean registered = userHandler.register(username, password, email);
+        boolean loggedIn = userHandler.login(username, password);
 
         System.out.println("registered? " + registered);
         System.out.println("Login erfolgreich? " + loggedIn);
-        User fromRepo = userRepository.getUserByUsername(user.getUsername());
-        System.out.println("Token im Repo: " + fromRepo.getToken());
-        System.out.println("Token im lokalen Objekt: " + user.getToken());
 
-
-        // === Game Setup ===
-        IGameRepository gameRepository = GameSqlRepository.getInstance(); // Liste als "DB"
-        IGameService gameService = new GameService(gameRepository);
-        Gamehandler gameHandler = new Gamehandler(gameService);
 
 
     }
+
 }
